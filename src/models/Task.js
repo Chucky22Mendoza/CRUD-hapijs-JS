e90fc37681b1eb7d6d13209332c831cc2679c699
Jesús Mendoza Verduzco <loginlock22@gmail.com>
2020-03-21 18:40:02 -0600

    ejemplo

---
+++ src/models/Task.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require('mongoose');
+
+const taskSchema = new Schema({
+    name: {
+        required: true,
+        type: String
+    },
+    description: String
+}, {
+    timestamps: true
+});
+
+module.exports = model('Task', taskSchema);
